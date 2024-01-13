3ac54501b48c16f249d2ea6e5bfb9adcc59ee3f6
Plamen <plamen.ivanov17@gmail.com>
2024-01-13 20:29:20 +0200

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	// custom hack used to disable CSS modules...
+	webpack(config) {
+		const oneOfRule = config.module.rules.find((rule) => rule.oneOf);
+		if (!oneOfRule) return console.log('Unable to find css module rule to disabled it');
+		oneOfRule.oneOf.forEach((one) => {
+			if (!(one.issuer && one.issuer.and && `${one.issuer.and}`.includes('_app'))) return;
+			one.issuer.and = [path.resolve(__dirname)];
+		});
+		return config;
+	}
+};
+
+module.exports = nextConfig;
